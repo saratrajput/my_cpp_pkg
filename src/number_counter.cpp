2644dab79742fa8a0ff8b82ab34a38f8e49480a7
pattarsuraj <pattarsuraj@gmail.com>
2021-02-22 19:19:46 +0900

    :zap: ROS2 Services

--- src/number_counter.cpp
+++ src/number_counter.cpp
@@ -1,6 +1,10 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
+#include "example_interfaces/srv/set_bool.hpp"
  
+using std::placeholders::_1;
+using std::placeholders::_2;
+
 class NumberCounterNode : public rclcpp::Node
 {
 public:
@@ -9,6 +13,10 @@ public:
         publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number_count", 10);
         subscriber_ = this->create_subscription<example_interfaces::msg::Int64>("number", 10,
                         std::bind(&NumberCounterNode::callbackNumberCounter, this, std::placeholders::_1));
+        // Reset counter service
+        server_ = this->create_service<example_interfaces::srv::SetBool>(
+                "reset_counter",
+                std::bind(&NumberCounterNode::callbackResetCounter, this, _1, _2));
         RCLCPP_INFO(this->get_logger(), "Number Counter node has started.");
     }
  
@@ -23,12 +31,29 @@ private:
         new_msg.data = counter_;
         publisher_->publish(new_msg);
     }
+
+    void callbackResetCounter(const example_interfaces::srv::SetBool::Request::SharedPtr request,
+                              const example_interfaces::srv::SetBool::Response::SharedPtr response)
+    {
+        if (request->data)
+        {
+            counter_ = 0;
+            response->success = true;
+            response->message = "Counter has been reset";
+        }
+        else
+        {
+            response->success = false;
+            response->message = "Counter has not been reset";
+        }
+    }
     int counter_;
     // Declare subscriber
     rclcpp::Subscription<example_interfaces::msg::Int64>::SharedPtr subscriber_;
     // Declare Publisher
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
-
+    // Declare Service
+    rclcpp::Service<example_interfaces::srv::SetBool>::SharedPtr server_;
 };
  
 int main(int argc, char **argv)
